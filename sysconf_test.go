ee01d9b2c737715358956279baf6fd97cc83a7fa
Tobias Klauser <tklauser@distanz.ch>
2018-09-27 17:26:38 +0200

    Rework tests to check against C.sysconf
    Rework the test to verify the result of sysconf.Sysconf against
    C.sysconf for each supported SC_* value. Works on Linux only for now.
    This requires moving the test to a separate module, so we can use cgo.

--- sysconf_test.go
+++ sysconf_test.go
@@ -5,47 +5,17 @@
 package sysconf_test
 
 import (
-	"os"
 	"testing"
 
 	"github.com/tklauser/go-sysconf"
 )
 
 func TestSysconf(t *testing.T) {
-	testCases := []struct {
-		val      int
-		name     string
-		expect   bool
-		expected int64
-	}{
-		// POSIX.1
-		{sysconf.SC_ARG_MAX, "ARG_MAX", false, 0},
-		{sysconf.SC_CHILD_MAX, "CHILD_MAX", false, 0},
-		{sysconf.SC_CLK_TCK, "clock ticks", false, 0},
-		{sysconf.SC_PAGESIZE, "PAGESIZE", true, int64(os.Getpagesize())},
-		{sysconf.SC_SYMLOOP_MAX, "SYMLOOP_MAX", false, 0},
-		{sysconf.SC_TZNAME_MAX, "TZNAME_MAX", false, 0},
-		{sysconf.SC_VERSION, "_POSIX_VERSION", false, 0},
-		// POSIX.2
-		{sysconf.SC_BC_BASE_MAX, "BC_BASE_MAX", false, 0},
-		{sysconf.SC_COLL_WEIGHTS_MAX, "COLL_WEIGHTS_MAX", false, 0},
-		{sysconf.SC_LINE_MAX, "LINE_MAX", false, 0},
-		{sysconf.SC_2_VERSION, "_POSIX2_VERSION", false, 0},
-		{sysconf.SC_2_C_DEV, "_POSIX2_C_DEV", false, 0},
-		// non-standard
-		{sysconf.SC_PHYS_PAGES, "number of pages of physical memory", false, 0},
-		{sysconf.SC_NPROCESSORS_CONF, "number of processors configured", false, 0},
-		{sysconf.SC_NPROCESSORS_ONLN, "number of processors online", false, 0},
+	// Just test the basic functionality here. The comparison tests against
+	// C.sysconf are in the test directory.
+	val, err := sysconf.Sysconf(sysconf.SC_CLK_TCK)
+	if err != nil {
+		t.Fatalf("Sysconf(CLK_TCK): %v", err)
 	}
-
-	for _, tc := range testCases {
-		val, err := sysconf.Sysconf(tc.val)
-		if err != nil {
-			t.Fatalf("Sysconf(%s/%d): %v", tc.name, tc.val, err)
-		}
-		t.Logf("%s = %v", tc.name, val)
-		if tc.expect && val != tc.expected {
-			t.Errorf("Sysconf(%s/%d):expected %v, got %v", tc.name, tc.val, tc.expected, val)
-		}
-	}
+	t.Logf("CLK_TCK = %v", val)
 }
