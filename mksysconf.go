tklauser/go-sysconf

commit 3011bd38196d484a09e7565f5892c069938f1e6d

mksysconf: allow to override GO{ARCH,OS} using GO{OS,ARCH}_TARGET env vars
Makes it easier to test change.

mksysconf.go
@@ -51,10 +51,18 @@ func gensysconf(in, out, goos, goarch string) error {
 }
 
 func main() {
-	goos, goarch := runtime.GOOS, runtime.GOARCH
-	if goos == "illumos" {
-		goos = "solaris"
+	goos := os.Getenv("GOOS_TARGET")
+	if goos == "" {
+		goos = runtime.GOOS
+		if goos == "illumos" {
+			goos = "solaris"
+		}
 	}
+	goarch := os.Getenv("GOARCH_TARGET")
+	if goarch == "" {
+		goarch = runtime.GOARCH
+	}
+
 	defs := fmt.Sprintf("sysconf_defs_%s.go", goos)
 	if err := gensysconf(defs, "z"+defs, goos, ""); err != nil {
 		fmt.Fprintln(os.Stderr, err)

commit ab20a1e8fac3c409471902eb9e2e0bf5d5cf8758

mksysconf: consistently use overriden GO{ARCH,OS}
Fixes: 3011bd38196d ("mksysconf: allow to override GO{ARCH,OS} using GO{OS,ARCH}_TARGET env vars")

mksysconf.go
@@ -69,9 +69,9 @@ func main() {
 		os.Exit(1)
 	}
 
-	vals := fmt.Sprintf("sysconf_values_%s.go", runtime.GOOS)
+	vals := fmt.Sprintf("sysconf_values_%s.go", goos)
 	// sysconf variable values are GOARCH-specific, thus write per GOARCH
-	zvals := fmt.Sprintf("zsysconf_values_%s_%s.go", runtime.GOOS, runtime.GOARCH)
+	zvals := fmt.Sprintf("zsysconf_values_%s_%s.go", goos, goarch)
 	if err := gensysconf(vals, zvals, goos, goarch); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)